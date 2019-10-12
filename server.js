31dbb9d50a4f2f81ba7254270ca7aed169380af9
Krithika Subramanian <ksubram2@students.kennesaw.edu>
2019-10-12 18:36:10 -0400

    changed port

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 8080;
-const fs = require('fs');
-const sequelize = require('sequelize');
+const fs = require("fs");
+const sequelize = require("sequelize");
 const db = require("./models");
 
 const app = express();
@@ -15,7 +15,12 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
 
-db.sequelize.sync({ force: true })
-  .then(() => app.listen(PORT, () => console.log('Listening on port %s', PORT)));
+db.sequelize
+  .sync({ force: true })
+  .then(() =>
+    app.listen(process.env.PORT || 3000, () =>
+      console.log("Listening on port %s", PORT)
+    )
+  );
 
 module.exports = app;
